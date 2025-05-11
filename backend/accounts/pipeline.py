078ceb16d663db370a5419cde5579dc17fb05ede
Alex <aria.alex2000@gmail.com>
2025-05-11 21:32:20 +0330

    update

--- backend/accounts/pipeline.py
+++
@@ -1,7 +0,0 @@
-from .models import UserProfile
-
-def create_user_profile(backend, user, response, *args, **kwargs):
-    """Create user profile for social auth users if it doesn't exist."""
-    if not hasattr(user, 'profile'):
-        UserProfile.objects.create(user=user)
-    return None 
